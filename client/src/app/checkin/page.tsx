b10043766fd48ede0c9113b65105523ba32e465c
Lincoln Lee <Lsha0730@gmail.com>
2025-05-17 13:40:13 -0700

    feat: single-use tabs

--- client/src/app/checkin/page.tsx
+++ client/src/app/checkin/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import axios from "axios";
-import { Suspense, useEffect, useRef, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { TbLogin } from "react-icons/tb";
 import RequireAuth from "../components/RequireAuth";
 import { useAuth } from "../context/AuthContext";
 import { useSearchParams } from "next/navigation";
 import { formatExpiration } from "../lib/time";
 
+export type Info = {
+  condition: boolean;
+  header: string;
+  message?: string;
+};
+
 const MS_IN_SEC = 1000;
 const MS_IN_MIN = 60 * MS_IN_SEC;
+const DEFAULT_DURATION = 30;
 
 export default function SuspensefulCheckIn() {
   return (
@@ -21,15 +28,16 @@ export default function SuspensefulCheckIn() {
 
 function CheckIn() {
   const { user } = useAuth();
-  const hasCheckedIn = useRef(false);
   const searchParams = useSearchParams();
   const [expiration, setExpiration] = useState<string | null>(null);
+  const [isNewTab, setIsNewTab] = useState<boolean>(true);
 
   useEffect(() => {
-    if (user && !hasCheckedIn.current) {
-      hasCheckedIn.current = true;
-
-      const durationParam = searchParams.get("duration") || "30";
+    const reqSent = sessionStorage.getItem("checkin_request_sent");
+    if (reqSent) setIsNewTab(false);
+    if (user && !reqSent) {
+      const durationParam =
+        searchParams.get("duration") || DEFAULT_DURATION.toString();
       const durationMinutes = durationParam ? parseInt(durationParam) : null;
       const expirationISO =
         durationMinutes &&
@@ -41,23 +49,34 @@ function CheckIn() {
         id: user.displayName,
         expiration: expirationISO ?? null,
       });
+      sessionStorage.setItem("checkin_request_sent", "true");
     }
   }, [user, searchParams]);
+
+  const formattedExpiration = formatExpiration(expiration as string);
+
+  const infochart: Info[] = [
+    {
+      condition: !isNewTab,
+      header: "Stale Tab",
+      message: `Last signed in until ${formattedExpiration}\nTap again or open a new tab!`,
+    },
+    {
+      condition: Boolean(user),
+      header: `Checked-in until ${formattedExpiration}!`,
+      message: `We notified everyone!\nRenew your time by tapping again.`,
+    },
+    { condition: true, header: "Loading..." },
+  ];
+  const info = infochart.find((e) => e.condition);
 
   return (
     <RequireAuth>
       <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4 text-black">
         <div className="bg-white rounded-2xl shadow-md p-8 flex flex-col items-center gap-6 w-full max-w-md text-center">
           <TbLogin className="w-24 h-24 text-green-600" />
-          <h1 className="text-2xl font-semibold">
-            {user && expiration
-              ? `Checked-in until ${formatExpiration(expiration)}!`
-              : "Loading..."}
-          </h1>
-          <p className="text-gray-600">
-            We’ve let everyone know you’re here. Renew your time by tapping
-            again.
-          </p>
+          <h1 className="text-2xl font-semibold">{info?.header}</h1>
+          <p className="text-gray-600 whitespace-pre-line">{info?.message}</p>
         </div>
       </div>
     </RequireAuth>
--- client/src/app/checkout/page.tsx
+++ client/src/app/checkout/page.tsx
@@ -1,34 +1,55 @@
 "use client";
 
 import axios from "axios";
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 import { TbLogout } from "react-icons/tb";
 import RequireAuth from "../components/RequireAuth";
 import { useAuth } from "../context/AuthContext";
+import { Info } from "../checkin/page";
+import { formatExpiration } from "../lib/time";
 
 export default function CheckOut() {
   const { user } = useAuth();
-  const hasCheckedOut = useRef(false);
+  const [isNewTab, setIsNewTab] = useState<boolean>(true);
+  const [checkoutTime, setCheckoutTime] = useState<string | null>(null);
 
   useEffect(() => {
-    if (user && !hasCheckedOut.current) {
-      hasCheckedOut.current = true;
+    const reqSent = sessionStorage.getItem("checkout_request_sent");
+    if (reqSent) setIsNewTab(false);
+    if (user && !reqSent) {
       const url = `${process.env.NEXT_PUBLIC_SERVER_URL}/checkout`;
       axios.post(url, { id: user.displayName });
+      sessionStorage.setItem("checkout_time_iso", new Date().toISOString());
+      sessionStorage.setItem("checkout_request_sent", "true");
     }
+
+    setCheckoutTime(sessionStorage.getItem("checkout_time_iso"));
   }, [user]);
+
+  const infochart: Info[] = [
+    {
+      condition: !isNewTab,
+      header: "Stale Tab",
+      message:
+        (checkoutTime && `Checked out at ${formatExpiration(checkoutTime)}\n`) +
+        "Tap again or open a new tab!",
+    },
+    {
+      condition: Boolean(user),
+      header: `See you later, ${user?.displayName}!`,
+      message: "You've been removed from the roster.\nCome back soon!",
+    },
+    { condition: true, header: "Loading..." },
+  ];
+  const info = infochart.find((e) => e.condition);
 
   return (
     <RequireAuth>
       <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4 text-black">
         <div className="bg-white rounded-2xl shadow-md p-8 flex flex-col items-center gap-6 w-full max-w-md text-center">
           <TbLogout className="w-24 h-24 text-red-500" />
-          <h1 className="text-2xl font-semibold">
-            {user ? `See you later, ${user.displayName}!` : "Loading..."}
-          </h1>
-          <p className="text-gray-600">
-            You’ve been removed from the roster. Come back soon!
-          </p>
+          <h1 className="text-2xl font-semibold">{info?.header}</h1>
+          <p className="text-gray-600 whitespace-pre-line">{info?.message}</p>
         </div>
       </div>
     </RequireAuth>
